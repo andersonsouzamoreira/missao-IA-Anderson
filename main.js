bc017494f04ed328fb2933e2de3c03bcaa409e72
andersonsouzamoreira <147444027+andersonsouzamoreira@users.noreply.github.com>
2025-10-07 14:57:38 -0300

    08/25

--- main.js
+++ main.js
@@ -82,7 +82,15 @@ function mostraPergunta(){
 }
 
 function mostraAlternativas(){
-    
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click',function(){
+            atual++
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
 
